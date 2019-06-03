9290146672597508d4b8bc5eb355acd83e76ba96
artem-v <artem.vysochyn@gmail.com>
2019-06-03 18:42:59 +0300

    Added hashCode/equals

--- aeron-cluster-poc-examples/src/main/java/io/scalecube/acpoc/Address.java
+++ aeron-cluster-poc-examples/src/main/java/io/scalecube/acpoc/Address.java
@@ -1,5 +1,7 @@
 package io.scalecube.acpoc;
 
+import java.util.Objects;
+
 public class Address {
 
   private final String host;
@@ -20,6 +22,23 @@ public class Address {
 
   public int port() {
     return port;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Address address = (Address) o;
+    return port == address.port && host.equals(address.host);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(host, port);
   }
 
   @Override
