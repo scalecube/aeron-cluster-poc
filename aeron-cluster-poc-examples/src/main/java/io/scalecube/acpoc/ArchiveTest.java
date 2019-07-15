fa83b66377abd0a96fd79f0a42c6fa02dae134c2
Artem Vysochyn <artem.vysochyn@gmail.com>
2019-07-15 22:02:38 +0300

    Experiemnt with aeron archive

---
+++ aeron-cluster-poc-examples/src/main/java/io/scalecube/acpoc/ArchiveTest.java
@@ -0,0 +1,122 @@
+package io.scalecube.acpoc;
+
+import io.aeron.Aeron;
+import io.aeron.Aeron.Context;
+import io.aeron.CommonContext;
+import io.aeron.ConcurrentPublication;
+import io.aeron.Subscription;
+import io.aeron.archive.Archive;
+import io.aeron.archive.ArchiveThreadingMode;
+import io.aeron.archive.client.AeronArchive;
+import io.aeron.archive.codecs.SourceLocation;
+import io.aeron.driver.MediaDriver;
+import io.aeron.driver.ThreadingMode;
+import java.nio.file.Paths;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.locks.LockSupport;
+import org.agrona.concurrent.UnsafeBuffer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ArchiveTest {
+
+  public static final Logger LOGGER = LoggerFactory.getLogger(ArchiveTest.class);
+
+  public static final int RECORDING_STREAM_ID = 100;
+  public static final int REPLAY_STREAM_ID = 200;
+
+  public static void main(String[] args) throws Exception {
+    String aeronDirectoryName =
+        Paths.get(CommonContext.getAeronDirectoryName(), "archiveTest").toString();
+
+    MediaDriver mediaDriver =
+        MediaDriver.launch(
+            new MediaDriver.Context()
+                .errorHandler(ex -> LOGGER.error("MediaDriver error: ", ex))
+                .dirDeleteOnStart(true)
+                .threadingMode(ThreadingMode.SHARED_NETWORK)
+                .aeronDirectoryName(aeronDirectoryName));
+
+    Archive archive =
+        Archive.launch(
+            new Archive.Context()
+                .errorHandler(ex1 -> LOGGER.error("Archive error: ", ex1))
+                .aeronDirectoryName(aeronDirectoryName)
+                .threadingMode(ArchiveThreadingMode.SHARED));
+
+    LOGGER.info("archiveDir: " + archive.context().archiveDir().getAbsolutePath());
+
+    AeronArchive aeronArchive =
+        AeronArchive.connect(
+            new AeronArchive.Context()
+                .errorHandler(ex -> LOGGER.error("AeronArchive error: ", ex))
+                .aeronDirectoryName(aeronDirectoryName));
+
+    Aeron aeron =
+        Aeron.connect(
+            new Context()
+                .aeronDirectoryName(aeronDirectoryName)
+                .errorHandler(ex -> LOGGER.error("Aeron error: ", ex)));
+
+    startNewRecording(aeronArchive, aeron);
+    String replayChannel = startNewReplay(aeronArchive, aeron);
+
+    Subscription replaySubscription = aeron.addSubscription(replayChannel, REPLAY_STREAM_ID);
+    LOGGER.info("created replaySubscription: {}, connecting ...", replaySubscription);
+    do {
+      LockSupport.parkNanos(1);
+    } while (!replaySubscription.isConnected());
+    LOGGER.info("replaySubscription connected: " + replaySubscription);
+
+    CountDownLatch latch = new CountDownLatch(2);
+
+    do {
+      int poll =
+          replaySubscription.poll(
+              (buffer, offset, length, header) -> {
+                byte[] bytes = new byte[length];
+                buffer.getBytes(offset, bytes);
+                LOGGER.info("### (buffer, offset, length, header) -> {}", new String(bytes));
+                latch.countDown();
+              },
+              100500);
+
+      LOGGER.info("replaySubscription.poll fragments received: " + poll);
+    } while (latch.getCount() != 0);
+  }
+
+  private static String startNewReplay(AeronArchive aeronArchive, Aeron aeron) {
+    ConcurrentPublication replayPublication =
+        aeron.addPublication(CommonContext.IPC_CHANNEL, REPLAY_STREAM_ID);
+    String replayChannel = replayPublication.channel();
+    LOGGER.info("replayPublication: {}", replayPublication);
+
+    RecordingDescriptor lastRecording = findLastRecording(aeronArchive);
+
+    long replaySessionId =
+        aeronArchive.startReplay(
+            lastRecording.recordingId, 0, Long.MAX_VALUE, replayChannel, REPLAY_STREAM_ID);
+    LOGGER.info("aeronArchive.startReplay replaySessionId: " + replaySessionId);
+    return replayChannel;
+  }
+
+  private static void startNewRecording(AeronArchive aeronArchive, Aeron aeron) {
+    ConcurrentPublication recordingPublication =
+        aeron.addPublication(CommonContext.IPC_CHANNEL, RECORDING_STREAM_ID);
+    long recording =
+        aeronArchive.startRecording(
+            recordingPublication.channel(), RECORDING_STREAM_ID, SourceLocation.LOCAL);
+
+    LOGGER.info("aeronArchive.startRecording: {}, pub: {}", recording, recordingPublication);
+    long offer1 = recordingPublication.offer(new UnsafeBuffer(("hello world 1").getBytes()));
+    long offer2 = recordingPublication.offer(new UnsafeBuffer(("hello world 2").getBytes()));
+    LOGGER.info("startNewRecording: recording offer1: {}, offer2: {}", offer1, offer2);
+  }
+
+  private static RecordingDescriptor findLastRecording(AeronArchive aeronArchive) {
+    RecordingDescriptor lastRecording =
+        AeronArchiveUtil.findLastRecording(aeronArchive, RECORDING_STREAM_ID);
+    LOGGER.info("aeronArchiveUtil.findLastRecording: " + lastRecording);
+    return lastRecording;
+  }
+}
