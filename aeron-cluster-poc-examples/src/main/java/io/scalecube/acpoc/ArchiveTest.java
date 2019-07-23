e1c46d6ffdc339c401c36d3e2e5ceee23a73617b
Artem Vysochyn <artem.vysochyn@gmail.com>
2019-07-23 11:23:53 +0300

    WIP on taggs

--- aeron-cluster-poc-examples/src/main/java/io/scalecube/acpoc/ArchiveTest.java
+++ aeron-cluster-poc-examples/src/main/java/io/scalecube/acpoc/ArchiveTest.java
@@ -3,6 +3,7 @@ package io.scalecube.acpoc;
 import io.aeron.Aeron;
 import io.aeron.Aeron.Context;
 import io.aeron.ChannelUri;
+import io.aeron.ChannelUriStringBuilder;
 import io.aeron.CommonContext;
 import io.aeron.ConcurrentPublication;
 import io.aeron.Subscription;
@@ -13,7 +14,7 @@ import io.aeron.archive.codecs.SourceLocation;
 import io.aeron.driver.MediaDriver;
 import io.aeron.driver.ThreadingMode;
 import java.nio.file.Paths;
-import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.LockSupport;
 import org.agrona.concurrent.UnsafeBuffer;
 import org.slf4j.Logger;
@@ -26,85 +27,98 @@ public class ArchiveTest {
   public static final int RECORDING_STREAM_ID = 100;
   public static final int REPLAY_STREAM_ID = 200;
 
-  /** Archive test runner. */
-  public static void main(String[] args) throws Exception {
+  public static void main(String[] args) {
     String aeronDirectoryName =
         Paths.get(CommonContext.getAeronDirectoryName(), "archiveTest").toString();
 
-    try (MediaDriver mediaDriver =
-            MediaDriver.launch(
-                new MediaDriver.Context()
-                    .errorHandler(ex -> LOGGER.error("MediaDriver error: ", ex))
-                    .dirDeleteOnStart(true)
-                    .threadingMode(ThreadingMode.SHARED_NETWORK)
-                    .aeronDirectoryName(aeronDirectoryName));
-        Archive archive =
-            Archive.launch(
-                new Archive.Context()
-                    .errorHandler(ex1 -> LOGGER.error("Archive error: ", ex1))
-                    .aeronDirectoryName(aeronDirectoryName)
-                    .threadingMode(ArchiveThreadingMode.SHARED));
-        AeronArchive aeronArchive =
-            AeronArchive.connect(
-                new AeronArchive.Context()
-                    .errorHandler(ex -> LOGGER.error("AeronArchive error: ", ex))
-                    .aeronDirectoryName(aeronDirectoryName));
-        Aeron aeron =
-            Aeron.connect(
-                new Context()
-                    .aeronDirectoryName(aeronDirectoryName)
-                    .errorHandler(ex -> LOGGER.error("Aeron error: ", ex))); ) {
+    MediaDriver mediaDriver =
+        MediaDriver.launch(
+            new MediaDriver.Context()
+                .errorHandler(ex -> LOGGER.error("MediaDriver error: ", ex))
+                .dirDeleteOnStart(true)
+                .threadingMode(ThreadingMode.SHARED_NETWORK)
+                .aeronDirectoryName(aeronDirectoryName));
 
-      LOGGER.info("archiveDir: " + archive.context().archiveDir().getAbsolutePath());
+    Archive archive =
+        Archive.launch(
+            new Archive.Context()
+                .errorHandler(ex1 -> LOGGER.error("Archive error: ", ex1))
+                .aeronDirectoryName(aeronDirectoryName)
+                .threadingMode(ArchiveThreadingMode.SHARED));
 
-      startNewRecording(aeronArchive, aeron);
+    LOGGER.info("archiveDir: " + archive.context().archiveDir().getAbsolutePath());
 
-      ConcurrentPublication replayPublication =
-          aeron.addPublication(
-              ChannelUri.addSessionId(CommonContext.IPC_CHANNEL, 42), REPLAY_STREAM_ID);
+    AeronArchive aeronArchive =
+        AeronArchive.connect(
+            new AeronArchive.Context()
+                .errorHandler(ex -> LOGGER.error("AeronArchive error: ", ex))
+                .aeronDirectoryName(aeronDirectoryName));
 
-      String replayChannel = startNewReplay(aeronArchive, replayPublication);
+    Aeron aeron =
+        Aeron.connect(
+            new Context()
+                .aeronDirectoryName(aeronDirectoryName)
+                .errorHandler(ex -> LOGGER.error("Aeron error: ", ex)));
 
-      Subscription replaySubscription = aeron.addSubscription(replayChannel, REPLAY_STREAM_ID);
-      LOGGER.info("created replaySubscription: {}, connecting ...", replaySubscription);
-      do {
-        LockSupport.parkNanos(1);
-      } while (!replaySubscription.isConnected());
-      LOGGER.info(
-          "replaySubscription connected: {}, images: {}",
-          replaySubscription,
-          replaySubscription.images());
+    startNewRecording(aeronArchive, aeron);
 
-      CountDownLatch latch = new CountDownLatch(2);
+    int sessionId = 42;
+    String publicationChannel =
+        new ChannelUriStringBuilder()
+            .media("udp")
+            .endpoint("localhost:4000")
+            .sessionId(sessionId)
+            .tags("" + sessionId)
+            //.isSessionIdTagged(true)
+            .build();
 
-      do {
-        int poll =
-            replaySubscription.poll(
-                (buffer, offset, length, header) -> {
-                  byte[] bytes = new byte[length];
-                  buffer.getBytes(offset, bytes);
-                  LOGGER.info(
-                      "### (buffer, offset, length, header) -> '{}' on sessionId: {}, streamId: {}",
-                      new String(bytes),
-                      header.sessionId(),
-                      header.streamId());
-                  latch.countDown();
-                },
-                100500);
+    ConcurrentPublication replayPublication =
+        aeron.addPublication(publicationChannel, REPLAY_STREAM_ID);
 
-        if (poll > 0) {
-          LOGGER.info("replaySubscription.poll fragments received: " + poll);
-        }
-      } while (latch.getCount() != 0);
-    }
+    String replayChannel = startNewReplay(aeronArchive, replayPublication);
+
+    Subscription replaySubscription = aeron.addSubscription(replayChannel, REPLAY_STREAM_ID);
+    LOGGER.info("created replaySubscription: {}, connecting ...", replaySubscription);
+    do {
+      LockSupport.parkNanos(1);
+    } while (!replaySubscription.isConnected());
+    LOGGER.info(
+        "replaySubscription connected: {}, images: {}",
+        replaySubscription,
+        replaySubscription.images());
+
+    pollSubscription(replaySubscription);
+    offerReplayPublication(replayPublication);
+  }
+
+  private static int poll(Subscription replaySubscription) {
+    return replaySubscription.poll(
+        (buffer, offset, length, header) -> {
+          byte[] bytes = new byte[length];
+          buffer.getBytes(offset, bytes);
+          LOGGER.info(
+              "### (buffer, offset, length, header) -> '{}' on sessionId: {}, streamId: {}",
+              new String(bytes),
+              header.sessionId(),
+              header.streamId());
+        },
+        100500);
   }
 
   private static String startNewReplay(
       AeronArchive aeronArchive, ConcurrentPublication replayPublication) {
-    String replayChannel = replayPublication.channel();
-    LOGGER.info("replayPublication: {}", replayPublication);
+    LOGGER.info("### startNewReplay: pub: {}", replayPublication);
 
     RecordingDescriptor lastRecording = findLastRecording(aeronArchive);
+
+    String replayChannel =
+        new ChannelUriStringBuilder()
+            .media("udp")
+            .endpoint("localhost:4000")
+            .sessionId(replayPublication.sessionId())
+            .isSessionIdTagged(true)
+            .tags("" + replayPublication.sessionId())
+            .build();
 
     long replaySessionId =
         aeronArchive.startReplay(
@@ -122,9 +136,12 @@ public class ArchiveTest {
             recordingPublication.channel(), RECORDING_STREAM_ID, SourceLocation.LOCAL);
 
     LOGGER.info("aeronArchive.startRecording: {}, pub: {}", recording, recordingPublication);
-    long offer1 = recordingPublication.offer(new UnsafeBuffer(("hello world 1").getBytes()));
-    long offer2 = recordingPublication.offer(new UnsafeBuffer(("hello world 2").getBytes()));
-    LOGGER.info("startNewRecording: recording offer1: {}, offer2: {}", offer1, offer2);
+
+    offerRecordingPublication(recordingPublication);
+
+    //    long offer1 = recordingPublication.offer(new UnsafeBuffer(("hello world 1").getBytes()));
+    //    long offer2 = recordingPublication.offer(new UnsafeBuffer(("hello world 2").getBytes()));
+    //    LOGGER.info("startNewRecording: recording offer1: {}, offer2: {}", offer1, offer2);
   }
 
   private static RecordingDescriptor findLastRecording(AeronArchive aeronArchive) {
@@ -132,5 +149,55 @@ public class ArchiveTest {
         AeronArchiveUtil.findLastRecording(aeronArchive, RECORDING_STREAM_ID);
     LOGGER.info("aeronArchiveUtil.findLastRecording: " + lastRecording);
     return lastRecording;
+  }
+
+  private static void offerReplayPublication(ConcurrentPublication replayPublication) {
+    new Thread(
+            () -> {
+              while (true) {
+                try {
+                  TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                  e.printStackTrace();
+                }
+                long offer =
+                    replayPublication.offer(
+                        new UnsafeBuffer(("hello world from replay publication 1").getBytes()));
+                LOGGER.info("replayPublication.offer: " + offer);
+              }
+            })
+        .start();
+  }
+
+  private static void pollSubscription(Subscription replaySubscription) {
+    new Thread(
+            () -> {
+              while (true) {
+                LockSupport.parkNanos(1);
+                int poll = poll(replaySubscription);
+                if (poll > 0) {
+                  LOGGER.info("replaySubscription.poll fragments received: " + poll);
+                }
+              }
+            })
+        .start();
+  }
+
+  private static void offerRecordingPublication(ConcurrentPublication recordingPublication) {
+    new Thread(
+            () -> {
+              while (true) {
+                try {
+                  TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                  e.printStackTrace();
+                }
+                long offer =
+                    recordingPublication.offer(
+                        new UnsafeBuffer(("recording " + System.currentTimeMillis()).getBytes()));
+                LOGGER.info("recordingPublication.offer: " + offer);
+              }
+            })
+        .start();
   }
 }
