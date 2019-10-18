0228bc915af1cea3401d5e3381b814f470d1890f
Artem Vysochyn <artem.vysochyn@gmail.com>
2019-10-18 13:18:29 +0300

    Tweaked node-0 node-1

--- aeron-cluster-poc-examples/scripts/node-0.sh
+++ aeron-cluster-poc-examples/scripts/node-0.sh
@@ -9,16 +9,16 @@ echo $JAR_FILE
 
 java \
   -cp target/${JAR_FILE}:target/lib/* \
--Daeron.archive.control.channel="aeron:udp?term-length=64k|endpoint=localhost:8010" \
+-Daeron.archive.control.channel="aeron:udp?term-length=64k|endpoint=mpp-vysochyn:8010" \
 -Daeron.archive.control.stream.id="100" \
--Daeron.archive.control.response.channel="aeron:udp?term-length=64k|endpoint=localhost:8020" \
+-Daeron.archive.control.response.channel="aeron:udp?term-length=64k|endpoint=mpp-vysochyn:8020" \
 -Daeron.archive.control.response.stream.id="100" \
--Daeron.archive.recording.events.channel="aeron:udp?control-mode=dynamic|control=localhost:8030" \
+-Daeron.archive.recording.events.channel="aeron:udp?control-mode=dynamic|control=mpp-vysochyn:8030" \
 -Daeron.archive.local.control.channel="aeron:ipc?term-length=64k" \
 -Daeron.cluster.member.id="0" \
--Daeron.cluster.members="0,localhost:20110,localhost:20220,localhost:20330,localhost:20440,localhost:8010|1,localhost:20111,localhost:20221,localhost:20331,localhost:20441,localhost:8011|2,localhost:20112,localhost:20222,localhost:20332,localhost:20442,localhost:8012" \
+-Daeron.cluster.members="0,mpp-vysochyn:20110,mpp-vysochyn:20220,mpp-vysochyn:20330,mpp-vysochyn:20440,mpp-vysochyn:8010|1,mpp-habryiel:20111,mpp-habryiel:20221,mpp-habryiel:20331,mpp-habryiel:20441,mpp-habryiel:8011
 -Daeron.cluster.ingress.channel="aeron:udp?term-length=64k" \
--Daeron.cluster.log.channel="aeron:udp?term-length=256k|control-mode=manual|control=localhost:20550" \
+-Daeron.cluster.log.channel="aeron:udp?term-length=256k|control-mode=manual|control=mpp-vysochyn:20550" \
 -Dio.scalecube.acpoc.instanceId=n0 \
 -Dio.scalecube.acpoc.cleanStart=false \
 -Dio.scalecube.acpoc.cleanShutdown=false \
--- aeron-cluster-poc-examples/scripts/node-1.sh
+++ aeron-cluster-poc-examples/scripts/node-1.sh
@@ -9,16 +9,16 @@ echo $JAR_FILE
 
 java \
   -cp target/${JAR_FILE}:target/lib/* \
--Daeron.archive.control.channel="aeron:udp?term-length=64k|endpoint=localhost:8011" \
+-Daeron.archive.control.channel="aeron:udp?term-length=64k|endpoint=mpp-habryiel:8011" \
 -Daeron.archive.control.stream.id="100" \
--Daeron.archive.control.response.channel="aeron:udp?term-length=64k|endpoint=localhost:8021" \
+-Daeron.archive.control.response.channel="aeron:udp?term-length=64k|endpoint=mpp-habryiel:8021" \
 -Daeron.archive.control.response.stream.id="101" \
--Daeron.archive.recording.events.channel="aeron:udp?control-mode=dynamic|control=localhost:8031" \
+-Daeron.archive.recording.events.channel="aeron:udp?control-mode=dynamic|control=mpp-habryiel:8031" \
 -Daeron.archive.local.control.channel="aeron:ipc?term-length=64k" \
 -Daeron.cluster.member.id="1" \
--Daeron.cluster.members="0,localhost:20110,localhost:20220,localhost:20330,localhost:20440,localhost:8010|1,localhost:20111,localhost:20221,localhost:20331,localhost:20441,localhost:8011|2,localhost:20112,localhost:20222,localhost:20332,localhost:20442,localhost:8012" \
+-Daeron.cluster.members="0,mpp-vysochyn:20110,mpp-vysochyn:20220,mpp-vysochyn:20330,mpp-vysochyn:20440,mpp-vysochyn:8010|1,mpp-habryiel:20111,mpp-habryiel:20221,mpp-habryiel:20331,mpp-habryiel:20441,mpp-habryiel:8011
 -Daeron.cluster.ingress.channel="aeron:udp?term-length=64k" \
--Daeron.cluster.log.channel="aeron:udp?term-length=256k|control-mode=manual|control=localhost:20551" \
+-Daeron.cluster.log.channel="aeron:udp?term-length=256k|control-mode=manual|control=mpp-habryiel:20551" \
 -Dio.scalecube.acpoc.instanceId=n1 \
 -Dio.scalecube.acpoc.cleanStart=false \
 -Dio.scalecube.acpoc.cleanShutdown=false \
