31ab158f3048b3651e2dfbc778f4fcc73700a441
Artem Vysochyn <artem.vysochyn@gmail.com>
2019-10-23 17:52:24 +0300

    Updated scripts

--- aeron-cluster-poc-examples/scripts/client-interactive.sh
+++ aeron-cluster-poc-examples/scripts/client-interactive.sh
@@ -9,7 +9,7 @@ echo $JAR_FILE
 
 java \
 -cp target/${JAR_FILE}:target/lib/* \
--Daeron.cluster.member.endpoints="0=mpp-vysochyn:20110,1=mpp-habryiel:20111,2=om2-savchuk:20112" \
+-Daeron.cluster.member.endpoints="0=node0:20110,1=node1:20111,2=node2:20112" \
 -Dnetworkaddress.cache.ttl=0 \
 -Dnetworkaddress.cache.negative.ttl=0 \
 ${JVM_OPTS} io.scalecube.acpoc.InteractiveClient
--- aeron-cluster-poc-examples/scripts/node-0.sh
+++ aeron-cluster-poc-examples/scripts/node-0.sh
@@ -9,16 +9,19 @@ echo $JAR_FILE
 
 java \
 -cp target/${JAR_FILE}:target/lib/* \
--Daeron.archive.control.channel="aeron:udp?term-length=64k|endpoint=mpp-vysochyn:8010" \
+-Daeron.archive.control.channel="aeron:udp?term-length=64k|endpoint=node0:8010" \
 -Daeron.archive.control.stream.id="100" \
--Daeron.archive.control.response.channel="aeron:udp?term-length=64k|endpoint=mpp-vysochyn:8020" \
+-Daeron.archive.control.response.channel="aeron:udp?term-length=64k|endpoint=node0:8020" \
 -Daeron.archive.control.response.stream.id="100" \
--Daeron.archive.recording.events.channel="aeron:udp?control-mode=dynamic|control=mpp-vysochyn:8030" \
+-Daeron.archive.recording.events.channel="aeron:udp?control-mode=dynamic|control=node0:8030" \
 -Daeron.archive.local.control.channel="aeron:ipc?term-length=64k" \
 -Daeron.cluster.member.id="0" \
--Daeron.cluster.members="0,mpp-vysochyn:20110,mpp-vysochyn:20220,mpp-vysochyn:20330,mpp-vysochyn:20440,mpp-vysochyn:8010|1,mpp-habryiel:20111,mpp-habryiel:20221,mpp-habryiel:20331,mpp-habryiel:20441,mpp-habryiel:8011|2,om2-savchuk:20112,om2-savchuk:20222,om2-savchuk:20332,om2-savchuk:20442,om2-savchuk:8012" \
+-Daeron.cluster.members="\
+0,node0:20110,node0:20220,node0:20330,node0:20440,node0:8010|\
+1,node1:20111,node1:20221,node1:20331,node1:20441,node1:8011|\
+2,node2:20112,node2:20222,node2:20332,node2:20442,node2:8012" \
 -Daeron.cluster.ingress.channel="aeron:udp?term-length=64k" \
--Daeron.cluster.log.channel="aeron:udp?term-length=256k|control-mode=manual|control=mpp-vysochyn:20550" \
+-Daeron.cluster.log.channel="aeron:udp?term-length=256k|control-mode=manual|control=node0:20550" \
 -Dio.scalecube.acpoc.instanceId=n0 \
 -Dio.scalecube.acpoc.cleanStart=false \
 -Dio.scalecube.acpoc.cleanShutdown=false \
