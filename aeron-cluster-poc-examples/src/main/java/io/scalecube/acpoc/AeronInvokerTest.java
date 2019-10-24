9a2a52bf6780480708caace4438f49b01f2b98c1
Artem Vysochyn <artem.vysochyn@gmail.com>
2019-10-24 20:21:04 +0300

    Started aeron invoker thread model investigation

--- aeron-cluster-poc-examples/src/main/java/io/scalecube/acpoc/AeronInvokerTest.java
+++ aeron-cluster-poc-examples/src/main/java/io/scalecube/acpoc/AeronInvokerTest.java
@@ -1,9 +1,6 @@
 package io.scalecube.acpoc;
 
-import static io.aeron.driver.status.SystemCounterDescriptor.SYSTEM_COUNTER_TYPE_ID;
-
 import io.aeron.Aeron;
-import io.aeron.Counter;
 import io.aeron.archive.Archive;
 import io.aeron.archive.ArchiveThreadingMode;
 import io.aeron.archive.client.AeronArchive;
@@ -11,8 +8,6 @@ import io.aeron.cluster.ConsensusModule;
 import io.aeron.driver.MediaDriver;
 import io.aeron.driver.MediaDriver.Context;
 import io.aeron.driver.ThreadingMode;
-import java.lang.reflect.Constructor;
-import org.agrona.concurrent.Agent;
 import org.agrona.concurrent.AgentRunner;
 import org.agrona.concurrent.BusySpinIdleStrategy;
 import org.agrona.concurrent.CompositeAgent;
@@ -42,8 +37,8 @@ public class AeronInvokerTest {
                 .useConductorAgentInvoker(true)
                 .idleStrategy(new BusySpinIdleStrategy()));
 
-    Counter archiveErrors = aeron.addCounter(SYSTEM_COUNTER_TYPE_ID, "Archive errors");
-    Counter clusterErrors = aeron.addCounter(SYSTEM_COUNTER_TYPE_ID, "Cluster errors");
+    //    Counter archiveErrors = aeron.addCounter(SYSTEM_COUNTER_TYPE_ID, "Archive errors");
+    //    Counter clusterErrors = aeron.addCounter(SYSTEM_COUNTER_TYPE_ID, "Cluster errors");
 
     // aeronArchiveContext
     AeronArchive.Context aeronArchiveContext =
@@ -53,8 +48,8 @@ public class AeronInvokerTest {
     Archive archive =
         Archive.launch(
             new Archive.Context()
-                .aeron(aeron)
-                .errorCounter(archiveErrors)
+                //                .aeron(aeron)
+                //                .errorCounter(archiveErrors)
                 .errorHandler(ex -> LOGGER.error("Exception occurred on Archive: ", ex))
                 .aeronDirectoryName(aeronDirectoryName)
                 .controlChannel(aeronArchiveContext.controlRequestChannel())
@@ -65,34 +60,25 @@ public class AeronInvokerTest {
                 .mediaDriverAgentInvoker(mediaDriver.sharedAgentInvoker())
                 .deleteArchiveOnStart(true));
 
-    // archiveInvoker
-
-    // consensusModuleContext
-    ConsensusModule.Context consensusModuleContext =
-        new ConsensusModule.Context()
-            .aeron(aeron)
-            .errorCounter(clusterErrors)
-            .errorHandler(ex -> LOGGER.error("Exception occurred on ConsensusModule: ", ex))
-            .aeronDirectoryName(aeronDirectoryName)
-            .archiveContext(aeronArchiveContext.clone());
-
-    consensusModuleContext.conclude();
-
-    Class<?> aclass = Class.forName("io.aeron.cluster.ConsensusModuleAgent");
-    Constructor<?> constructor = aclass.getDeclaredConstructor(ConsensusModule.Context.class);
-    constructor.setAccessible(true);
-    Agent consensusModuleAgent = (Agent) constructor.newInstance(consensusModuleContext);
-
-    AgentRunner agentRunner =
+    AgentRunner.startOnThread(
         new AgentRunner(
             new BusySpinIdleStrategy(),
             ex -> LOGGER.error("Exception occurred on AgentRunner: ", ex),
             null,
             new CompositeAgent(
-                mediaDriver.sharedAgentInvoker().agent(),
-                archive.invoker().agent(),
-                consensusModuleAgent));
-    AgentRunner.startOnThread(agentRunner);
+                mediaDriver.sharedAgentInvoker().agent(), archive.invoker().agent())));
+
+    // consensusModuleContext
+    ConsensusModule.Context consensusModuleContext =
+        new ConsensusModule.Context()
+            //            .aeron(aeron)
+            //            .errorCounter(clusterErrors)
+            .errorHandler(ex -> LOGGER.error("Exception occurred on ConsensusModule: ", ex))
+            .aeronDirectoryName(aeronDirectoryName)
+            .archiveContext(aeronArchiveContext.clone());
+
+    // consensusModule
+    ConsensusModule consensusModule = ConsensusModule.launch(consensusModuleContext);
 
     Thread.currentThread().join();
   }
