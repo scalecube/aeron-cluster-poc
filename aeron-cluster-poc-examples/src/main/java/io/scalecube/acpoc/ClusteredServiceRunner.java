03dadb4dc0a57d00a982eba9d2bafdc15fd1fd5c
Artem Vysochyn <artem.vysochyn@gmail.com>
2019-11-08 14:20:06 +0200

    Work in progreess on researching SHUTFOEWN

--- aeron-cluster-poc-examples/src/main/java/io/scalecube/acpoc/ClusteredServiceRunner.java
+++ aeron-cluster-poc-examples/src/main/java/io/scalecube/acpoc/ClusteredServiceRunner.java
@@ -1,5 +1,7 @@
 package io.scalecube.acpoc;
 
+import io.aeron.agent.EventConfiguration;
+import io.aeron.agent.EventLogAgent;
 import io.aeron.archive.Archive;
 import io.aeron.archive.ArchiveThreadingMode;
 import io.aeron.archive.client.AeronArchive;
@@ -8,12 +10,12 @@ import io.aeron.cluster.ConsensusModule;
 import io.aeron.cluster.ConsensusModule.Configuration;
 import io.aeron.cluster.service.ClusteredService;
 import io.aeron.cluster.service.ClusteredServiceContainer;
-import io.aeron.driver.DefaultAllowTerminationValidator;
 import io.aeron.driver.MediaDriver;
 import io.aeron.driver.MinMulticastFlowControlSupplier;
 import io.aeron.driver.ThreadingMode;
 import java.io.File;
 import java.nio.file.Paths;
+import net.bytebuddy.agent.ByteBuddyAgent;
 import org.agrona.CloseHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -33,6 +35,43 @@ public class ClusteredServiceRunner {
    * @param args arguments
    */
   public static void main(String[] args) {
+    //    System.setProperty(
+    //        EventConfiguration.ENABLED_EVENT_CODES_PROP_NAME,
+    //        "CMD_IN_ADD_PUBLICATION,"
+    //            + "CMD_IN_REMOVE_PUBLICATION,"
+    //            + "CMD_IN_ADD_SUBSCRIPTION,"
+    //            + "CMD_IN_REMOVE_SUBSCRIPTION,"
+    //            + "CMD_OUT_PUBLICATION_READY,"
+    //            + "CMD_OUT_AVAILABLE_IMAGE,"
+    //            + "INVOCATION,"
+    //            + "CMD_OUT_ON_OPERATION_SUCCESS,"
+    //            + "CMD_IN_KEEPALIVE_CLIENT,"
+    //            + "REMOVE_PUBLICATION_CLEANUP,"
+    //            + "REMOVE_SUBSCRIPTION_CLEANUP,"
+    //            + "REMOVE_IMAGE_CLEANUP,"
+    //            + "CMD_OUT_ON_UNAVAILABLE_IMAGE,"
+    //            + "SEND_CHANNEL_CREATION,"
+    //            + "RECEIVE_CHANNEL_CREATION,"
+    //            + "SEND_CHANNEL_CLOSE,"
+    //            + "RECEIVE_CHANNEL_CLOSE,"
+    //            + "CMD_IN_REMOVE_DESTINATION,"
+    //            + "CMD_IN_ADD_EXCLUSIVE_PUBLICATION,"
+    //            + "CMD_OUT_EXCLUSIVE_PUBLICATION_READY,"
+    //            + "CMD_OUT_ERROR,"
+    //            + "CMD_IN_ADD_COUNTER,"
+    //            + "CMD_IN_REMOVE_COUNTER,"
+    //            + "CMD_OUT_SUBSCRIPTION_READY,"
+    //            + "CMD_OUT_COUNTER_READY,"
+    //            + "CMD_OUT_ON_UNAVAILABLE_COUNTER,"
+    //            + "CMD_IN_CLIENT_CLOSE,"
+    //            + "CMD_IN_ADD_RCV_DESTINATION,"
+    //            + "CMD_IN_REMOVE_RCV_DESTINATION,"
+    //            + "CMD_OUT_ON_CLIENT_TIMEOUT,"
+    //            + "CMD_IN_TERMINATE_DRIVER");
+    System.setProperty(EventConfiguration.ENABLED_CLUSTER_EVENT_CODES_PROP_NAME, "all");
+    System.setProperty(EventConfiguration.ENABLED_ARCHIVE_EVENT_CODES_PROP_NAME, "all");
+    EventLogAgent.agentmain("", ByteBuddyAgent.install());
+
     String clusterMemberId = Integer.toHexString(Configuration.clusterMemberId());
     String nodeId = "node-" + clusterMemberId + "-" + Utils.instanceId();
     String nodeDirName = Paths.get("target", "aeron", "cluster", nodeId).toString();
@@ -47,8 +86,8 @@ public class ClusteredServiceRunner {
     MediaDriver.Context mediaDriverContext =
         new MediaDriver.Context()
             .errorHandler(ex -> logger.error("Exception occurred at MediaDriver: ", ex))
-            .terminationHook(() -> logger.info("TerminationHook called on MediaDriver "))
-            .terminationValidator(new DefaultAllowTerminationValidator())
+            // .terminationHook(() -> logger.info("TerminationHook called on MediaDriver "))
+            // .terminationValidator(new DefaultAllowTerminationValidator())
             .threadingMode(ThreadingMode.SHARED)
             .warnIfDirectoryExists(true)
             .dirDeleteOnStart(true)
@@ -70,7 +109,7 @@ public class ClusteredServiceRunner {
     ConsensusModule.Context consensusModuleContext =
         new ConsensusModule.Context()
             .errorHandler(ex -> logger.error("Exception occurred at ConsensusModule: ", ex))
-            .terminationHook(() -> logger.info("TerminationHook called on ConsensusModule"))
+            // .terminationHook(() -> logger.info("TerminationHook called on ConsensusModule"))
             .aeronDirectoryName(aeronDirectoryName)
             .clusterDir(new File(nodeDirName, "consensus"))
             .archiveContext(aeronArchiveContext.clone());
