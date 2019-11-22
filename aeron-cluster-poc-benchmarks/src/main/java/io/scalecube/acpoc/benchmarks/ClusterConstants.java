20f67468c3cd060231ab6123047b0952d68d1ae1
Artem Vysochyn <artem.vysochyn@gmail.com>
2019-11-22 14:57:06 +0200

    Added module benchmarlks

---
+++ aeron-cluster-poc-benchmarks/src/main/java/io/scalecube/acpoc/benchmarks/ClusterConstants.java
@@ -0,0 +1,27 @@
+package io.scalecube.acpoc.benchmarks;
+
+public class ClusterConstants {
+
+  public static final int LOG_CHANNEL_PORT_OFFSET = 1;
+  public static final int ARCHIVE_CONTROL_REQUEST_CHANNEL_PORT_OFFSET = 2;
+  public static final int ARCHIVE_CONTROL_RESPONSE_CHANNEL_PORT_OFFSET = 3;
+  public static final int ARCHIVE_RECORDING_EVENTS_CHANNEL_PORT_OFFSET = 4;
+  public static final int SERVICE_CLIENT_FACING_PORT_OFFSET = 5;
+  public static final int MEMBER_FACTING_PORT_OFFSET = 6;
+  public static final int LOG_PORT_OFFSET = 7;
+  public static final int TRANSFER_PORT_OFFSET = 8;
+  public static final int INGRESS_CHANNEL_PORT_OFFSET = 9;
+  public static final int EGRESS_CHANNEL_PORT_OFFSET = 10;
+  public static final int SNAPSHOT_CHANNEL_PORT_OFFSET = 11;
+  public static final int ORDER_EVENTS_CHANNEL_PORT_OFFSET = 12;
+
+  public static final int ARCHIVE_CONTROL_REQUEST_STREAM_ID_OFFSET = 100;
+  public static final int ARCHIVE_CONTROL_RESPONSE_STREAM_ID_OFFSET = 110;
+
+  public static final int SNAPSHOT_STREAM_ID = 120;
+  public static final int ORDER_EVENTS_STREAM_ID = 130;
+
+  private ClusterConstants() {
+    // Do not instantiate
+  }
+}
---
+++ aeron-cluster-poc-benchmarks/src/main/java/io/scalecube/acpoc/benchmarks/ClusterRoundTripBenchmark.java
@@ -0,0 +1,148 @@
+package io.scalecube.acpoc.benchmarks;
+
+import io.aeron.cluster.client.AeronCluster;
+import io.aeron.cluster.client.EgressListener;
+import io.aeron.logbuffer.Header;
+import java.util.concurrent.TimeUnit;
+import org.agrona.BitUtil;
+import org.agrona.BufferUtil;
+import org.agrona.CloseHelper;
+import org.agrona.DirectBuffer;
+import org.agrona.concurrent.UnsafeBuffer;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OperationsPerInvocation;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.TearDown;
+import org.openjdk.jmh.annotations.Threads;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.infra.Blackhole;
+
+@Fork(
+    value = 1,
+    jvmArgs = {
+      "-XX:+UnlockExperimentalVMOptions",
+      "-XX:+TrustFinalNonStaticFields",
+      "-XX:+UnlockDiagnosticVMOptions",
+      "-XX:GuaranteedSafepointInterval=300000",
+      "-XX:BiasedLockingStartupDelay=0",
+      "-XX:+UseParallelOldGC",
+      "-Dagrona.disable.bounds.checks=true",
+      "-Daeron.term.buffer.sparse.file=false",
+      "-Daeron.threading.mode=DEDICATED",
+      "-Daeron.shared.idle.strategy=org.agrona.concurrent.BusySpinIdleStrategy",
+      "-Daeron.sharednetwork.idle.strategy=org.agrona.concurrent.BusySpinIdleStrategy",
+      "-Daeron.sender.idle.strategy=org.agrona.concurrent.BusySpinIdleStrategy",
+      "-Daeron.receiver.idle.strategy=org.agrona.concurrent.BusySpinIdleStrategy",
+      "-Daeron.conductor.idle.strategy=org.agrona.concurrent.BusySpinIdleStrategy",
+      "-Daeron.archive.threading.mode=DEDICATED",
+      "-Daeron.archive.idle.strategy=org.agrona.concurrent.BusySpinIdleStrategy",
+      "-Daeron.archive.recorder.idle.strategy=org.agrona.concurrent.BusySpinIdleStrategy",
+      "-Daeron.archive.replayer.idle.strategy=org.agrona.concurrent.BusySpinIdleStrategy",
+      "-Daeron.cluster.idle.strategy=org.agrona.concurrent.BusySpinIdleStrategy",
+      "-Daeron.archive.file.sync.level=0",
+      "-Daeron.archive.segment.file.length=1g",
+      "-Daeron.archive.control.mtu.length=4k",
+      "-Daeron.spies.simulate.connection=true"
+    })
+@Threads(1)
+@Warmup(iterations = 3, time = 5)
+@Measurement(iterations = 5, time = 15)
+public class ClusterRoundTripBenchmark {
+
+  private static final int OPERATIONS_PER_INVOCATION = 1000;
+
+  /**
+   * Benchmark.
+   *
+   * @param state state
+   * @param blackhole blackhole
+   */
+  @Benchmark
+  @OutputTimeUnit(TimeUnit.NANOSECONDS)
+  @BenchmarkMode({Mode.SampleTime})
+  @OperationsPerInvocation(OPERATIONS_PER_INVOCATION)
+  public void testTime(BenchmarkState state, Blackhole blackhole) {
+    for (int i = 0; i < OPERATIONS_PER_INVOCATION; i++) {
+      blackhole.consume(state.run());
+    }
+  }
+
+  /**
+   * Benchmark.
+   *
+   * @param state state
+   * @param blackhole blackhole
+   */
+  @Benchmark
+  @OutputTimeUnit(TimeUnit.SECONDS)
+  @BenchmarkMode({Mode.Throughput})
+  @OperationsPerInvocation(OPERATIONS_PER_INVOCATION)
+  public void testTps(BenchmarkState state, Blackhole blackhole) {
+    for (int i = 0; i < OPERATIONS_PER_INVOCATION; i++) {
+      blackhole.consume(state.run());
+    }
+  }
+
+  @State(Scope.Benchmark)
+  public static class BenchmarkState implements EgressListener {
+
+    @Param({"256"})
+    private int messageLength;
+
+    private ClusterNode clusterNode;
+    private ClusterClient clusterClient;
+    private AeronCluster client;
+    private UnsafeBuffer offerBuffer;
+    private boolean receivedResponse;
+
+    long run() {
+      receivedResponse = false;
+      long result;
+      do {
+        result = client.offer(offerBuffer, 0, messageLength);
+      } while (result <= 0);
+      while (!receivedResponse) {
+        client.pollEgress();
+      }
+      return result;
+    }
+
+    /** Setup method. */
+    @Setup
+    public void setUp() {
+      clusterNode = ClusterNode.launch();
+      clusterClient = ClusterClient.launch(this);
+      client = clusterClient.client();
+
+      offerBuffer =
+          new UnsafeBuffer(
+              BufferUtil.allocateDirectAligned(messageLength, BitUtil.CACHE_LINE_LENGTH));
+    }
+
+    @Override
+    public void onMessage(
+        long clusterSessionId,
+        long timestamp,
+        DirectBuffer buffer,
+        int offset,
+        int length,
+        Header header) {
+      receivedResponse = true;
+    }
+
+    /** Tear down method. */
+    @TearDown
+    public void tearDown() {
+      CloseHelper.quietClose(clusterClient);
+      CloseHelper.quietClose(clusterNode);
+    }
+  }
+}
